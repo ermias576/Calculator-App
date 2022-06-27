44c230d2386962087951ee449a168484e0ca6384
ermias_tegegn <ermiastegegn576@gmail.com>
2022-06-27 23:55:50 +0300

    Calculator Commit

---
+++ ReadInput.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReadInput {
+
+    private final String regex = "([-+]?[0-9]*\\.?[0-9]+[\\/\\+\\-\\*])+([-+]?[0-9]*\\.?[0-9]+)";
+
+    public ReadInput() {
+    }
+
+    public boolean validateInput(String input) {
+        Pattern pattern = Pattern.compile(this.regex);
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.matches();
+    }
+
+    public static String read() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input expression (for eg: 4*3/2)");
+
+        String inputLine = scanner.nextLine();
+
+        ReadInput readInput = new ReadInput();
+
+        if (!readInput.validateInput(inputLine)) {
+            System.out.println("Mathematical expression validation failed");
+            System.exit(0);
+        }
+
+        return inputLine;
+    }
+}
